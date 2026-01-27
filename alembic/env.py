46e58f71f1dd0b5e31c51626ab7a59be2d1c7268
Saian <sayan.badarchy@gmail.com>
2026-01-28 01:02:51 +0400

    import game model

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from src.config import get_settings
 from src.database.core import Base
+from src.games.models import Game
 
 
 settings = get_settings()
